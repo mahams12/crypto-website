f9b186f405cce287cdc8a4e9d838d3d4f8f59d3c
jazim Saeed <jazimsaeed@jazims-MacBook-Air.local>
2025-09-10 21:00:41 +0500

    Update file

--- src/hooks/useCryptoData.js
+++ src/hooks/useCryptoData.js
@@ -1,49 +1,29 @@
 // src/hooks/useCryptoData.js
 import { useQuery } from "@tanstack/react-query";
-import { cryptoService } from "../services/api";
+import axios from "axios";
 
-// -----------------------
-// Get list of coins
-// -----------------------
-export const useCoins = (page = 1, limit = 20, sortBy = "market_cap_rank") => {
-  return useQuery({
-    queryKey: ["coins", page, limit, sortBy],
-    queryFn: () => cryptoService.getCoins(page, limit, sortBy),
-    select: (res) => res?.data,
-  });
+// Fetch crypto market data from CoinGecko
+const fetchCryptoData = async () => {
+  const response = await axios.get(
+    "https://api.coingecko.com/api/v3/coins/markets",
+    {
+      params: {
+        vs_currency: "usd",
+        order: "market_cap_desc",
+        per_page: 100,
+        page: 1,
+        sparkline: false,
+      },
+    }
+  );
+  return response.data;
 };
 
-// -----------------------
-// Get single coin details
-// -----------------------
-export const useCoin = (coinId) => {
+// Custom hook
+export function useCryptoData() {
   return useQuery({
-    queryKey: ["coin", coinId],
-    queryFn: () => cryptoService.getCoin(coinId),
-    enabled: !!coinId,
-    select: (res) => res?.data,
+    queryKey: ["cryptoData"],
+    queryFn: fetchCryptoData,
+    refetchInterval: 60000, // refresh every 60s
   });
-};
-
-// -----------------------
-// Get coin price history
-// -----------------------
-export const useCoinHistory = (coinId, days = 7) => {
-  return useQuery({
-    queryKey: ["coinHistory", coinId, days],
-    queryFn: () => cryptoService.getCoinHistory(coinId, days),
-    enabled: !!coinId,
-    select: (res) => res?.data,
-  });
-};
-
-// -----------------------
-// Get trending coins
-// -----------------------
-export const useTrendingCoins = (limit = 10) => {
-  return useQuery({
-    queryKey: ["trendingCoins", limit],
-    queryFn: () => cryptoService.getTrendingCoins(limit),
-    select: (res) => res?.data,
-  });
-};
+}
